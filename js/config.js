4df57bca77011bec02c5f884b1733ae8c4eba93b
natapolc-ux <natapol.c@chs.ac.th>
2026-09-22 13:33:04 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Matrix Student System V2
 // เปลี่ยน URL นี้เป็น Web App URL ของ Apps Script V2 หลัง deploy
-const API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
-const LOGIN_URL = 'PASTE_YOUR_CENTRAL_LOGIN_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwNY_ojt0kczg3zbcdypJ2g0C9HMm1R9agnzqGDl-O0Mm6h1Q3FjJ-V92uFb95eLtYFdw/exec';
+const LOGIN_URL = 'https://natapolc-ux.github.io/ClassFlow.V2/';
 const APP_PORTAL = 'teacher';
 const SESSION_KEY = 'matrix_v2_teacher_session';
